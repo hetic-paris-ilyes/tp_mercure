fc209d461788eeca5751d7971bbaada474ed2850
Giselle MM <giselle.magalong@gmail.com>
2022-12-05 20:02:15 +0100

    Create function chatMessage

--- symfony_project/src/Controller/ChatController.php
+++ symfony_project/src/Controller/ChatController.php
@@ -17,14 +17,43 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ChatController extends AbstractController
 {
-    #[Route('/create', name: 'chat_post', methods: 'POST')]
-    public function chatUser(User $user, HubInterface $hub)
+    #[Route('/getChatRoom', name: 'chat_test', methods: 'GET')]
+    public function getChatRoom(UserRepository $userRepository, ChatRepository $chatRepository, Request $request)
     {
-        // requete chat_user (1,2) existe ?  renvoie id room
-        // redirigier vers url /topic/idRoom
-        // si oui getmessages(idroom)
+        $user_1 = $_GET["userTo"];
+        $user_2 = $_GET["myUser"];
+        
+        $chatroom = $chatRepository->getChatsByUsers($user_1,$user_2);
+        if($chatroom !== null){
+            var_dump("Conversation existante");
+            $idChat = $chatroom["id"];
+            //return $this->redirectToRoute('chat_messages', ['id' => $idChat]);
+        }
+        else{
+            var_dump("Pas de conv");
+            $idChat = $chatRepository->newChat($user_1, $user_2, $userRepository);
+            //return $this->redirectToRoute('chat_messages', ['id' => $idChat]);
+        }
+        return $this->json([
+            'id_chat' => $idChat]);
+    }
 
-        $update = new Update(
+    #[Route('/chat/{id}', name: 'chat_messages', methods: 'GET')]
+    public function getChatMessages(ChatRepository     $chatRepository,
+                                    int             $id): \Symfony\Component\HttpFoundation\JsonResponse
+    {
+        return $this->json([
+            'chat' => $chatRepository->getAllMessagesOrderByDate($id)
+        ], 200, [], ['groups' => 'main']);
+    }
+
+    #[Route('/chat/{id}/createMessage', name: 'chat_post', methods: 'POST')]
+    public function chatMessage(HubInterface $hub,Request $request)
+    {
+        $post_data = json_decode($request->getContent(), true);
+        //$date = $post_data['date'];
+        var_dump($post_data);
+        /*$update = new Update(
             [
                 "https://example.com/my-private-topic",
                 "https://example.com/user/{$user->getId()}/?topic=" . urlencode("https://example.com/my-private-topic")
@@ -37,38 +66,10 @@ class ChatController extends AbstractController
         );
 
         $hub->publish($update);
-
+*/
         return $this->json([
             'message' => 'Ping sent'
         ]);
     }
 
-    #[Route('/test', name: 'chat_test', methods: 'GET')]
-    public function getChatRoom(UserRepository $userRepository, ChatRepository $chatRepository, Request $request)
-    {
-        $user_1 = $_GET["userTo"];
-        $user_2 = $_GET["myUser"];
-        
-        $chatroom = $chatRepository->getChatsByUsers($user_1,$user_2);
-        if($chatroom !== null){
-            var_dump("Conversation existante");
-            $idChat = $chatroom["id"];
-            return $this->redirectToRoute('chat_messages', ['id' => $idChat]);
-        }
-        else{
-            var_dump("Pas de conv");
-            $idChat = $chatRepository->newChat($user_1, $user_2, $userRepository);
-            return $this->redirectToRoute('chat_messages', ['id' => $idChat]);
-        }
-    }
-
-    #[Route('/chat/{id}', name: 'chat_messages', methods: 'GET')]
-    public function getChatMessages(ChatRepository     $chatRepository,
-                                    int             $id): \Symfony\Component\HttpFoundation\JsonResponse
-    {
-        $this->redirect("http://localhost:3000/chat/7")
-        return $this->json([
-            'chat' => $chatRepository->getAllMessagesOrderByDate(9)
-        ], 200, [], ['groups' => 'main']);
-    }
 }
